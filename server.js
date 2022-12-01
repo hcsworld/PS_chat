025686e2b4093429dfeffa1fabc096253f79a824
dlwns147 <lsj000777@naver.com>
2022-12-01 22:33:35 +0900

    add main and chat page

--- server.js
+++ server.js
@@ -21,6 +21,21 @@ app.get('/', (req, res) => {
     // res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/main', (req, res) => {
+    console.log('main')
+    // res.sendFile(__dirname + '/chatroom.html');
+    // res.sendFile(path.join(publicDirectoryPath, 'webpages', 'chatroom.html'));
+
+    fs.readFile(path.join(__dirname, 'webpages', 'main.html'), (error, data) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).send("<h1>500 Error</h1>");
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    });
+});
+
 app.get('/chat', (req, res) => {
     console.log('chat')
     // res.sendFile(__dirname + '/chatroom.html');
