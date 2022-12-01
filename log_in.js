5e1babc5397b0451e5d0d130a1f0025c06fae254
신현창 <sinhyeonchang@shc.local>
2022-12-01 20:52:20 +0900

    edit login js

--- log_in.js
+++ log_in.js
@@ -1,9 +1,11 @@
 let button = document.querySelector('#login');
 
 button.addEventListener("click", () => {
+    console.log("click!");
     let id = document.querySelector('#id');
     let password = document.querySelector('#password');
-
+    console.log(id.value);
+    console.log(password.value);
     if ((id.value === "student") && (password.value === "1234")) {
         location.replace("./studentpage.html");
     }
